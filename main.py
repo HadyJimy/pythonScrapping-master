35702f51977390cbe6daff7f662f5f3778081be0
hadyjimy <hadyg3733@gmail.com>
2022-12-17 20:47:59 +0200

    a

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 
 import requests
 from bs4 import BeautifulSoup
-links_list=['https://www.amazon.eg/s?k=iphone+14+pro+max&i=electronics&rh=n%3A21832883031%2Cp_n_operating_system_browse-bin%3A22079940031%2Cp_n_feature_four_browse-bin%3A26868023031&dc&qid=1671224182',
-            'https://www.amazon.eg/-/en/s?k=iphone+14+pro+max&i=electronics&rh=n%3A21832883031%2Cp_n_operating_system_browse-bin%3A22079940031%2Cp_n_feature_four_browse-bin%3A26868023031&dc&page=2&qid=1671284916&ref=sr_pg_2'        ]
+links_list= [
+'https://www.amazon.eg/s?k=iphone+14+pro+max&i=electronics&rh=n%3A21832883031%2Cp_n_operating_system_browse-bin%3A22079940031%2Cp_n_feature_four_browse-bin%3A26868023031&dc&qid=1671224182',
+ 'https://www.amazon.eg/-/en/s?k=iphone+14+pro+max&i=electronics&rh=n%3A21832883031%2Cp_n_operating_system_browse-bin%3A22079940031%2Cp_n_feature_four_browse-bin%3A26868023031&dc&page=2&qid=1671284916&ref=sr_pg_2'
+            ]
 counter=1
 r=0
 lis=[]
@@ -15,7 +17,7 @@ for i in links_list:
 
     for x in range(len(res)):
         print(f"{counter}-{res[x].text}")
-        print(f"rate : {rate[x].text}")
+        print(f" rate : {rate[x].text}")
         print(image[x].text)
         counter+=1
 
